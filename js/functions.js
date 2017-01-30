3d5d23b35f4b8b01d99c51a0f1f34db0a5accfb0
Mitchell Bartolo <mitchell.bartolo@gmail.com>
2017-01-30 23:07:52 +1000

    updated

---
+++ js/functions.js
@@ -0,0 +1,34 @@
+/**const hero = document.querySelector('.hero');
+const text = hero.querySelector('h1');
+const walk = 20;
+
+function shadow(e) {
+    //console.log(e);
+    const { offsetWidth: width, offsetHeight: height } = hero;
+    let { offsetX: x, offsetY: y } = e;
+
+    if (this !== e.target) {
+      //console.log(this.offsetLeft);
+      console.log(this, e.target.getBoundingClientRect().left);
+      //console.log(e.target);
+        //x = x + e.target.offsetLeft;
+        //y = y + e.target.offsetTop;
+        //x = e.target.getBoundingClientRect();
+        //y = e.target.getBoundingClientRect();
+    }
+
+
+    const xWalk = Math.round(x / width * walk) - (walk / 2);
+    const yWalk = Math.round(y / height * walk) - (walk / 2);
+
+    text.style.filter = `drop-shadow(${xWalk}px ${yWalk}px 1px grey)`;
+
+    //text.style.transform = `perspective( 5000px ) rotateX( ${yWalk}deg ) rotateY( ${xWalk}deg )`
+
+    //console.log(xWalk, yWalk);
+    //console.log(x, y);
+
+}
+
+hero.addEventListener('mousemove', shadow);
+**/
