1d44b72f33de0500dee92ba8649e2c43534c6687
Mitchell Bartolo <mitchell.bartolo@gmail.com>
2017-01-31 17:35:10 +1000

    added mouse shadow movement

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,6 @@
-/**const hero = document.querySelector('.hero');
+const hero = document.querySelector('.hero');
 const text = hero.querySelector('h1');
-const walk = 20;
+const walk = 30;
 
 function shadow(e) {
     //console.log(e);
@@ -9,10 +9,10 @@ function shadow(e) {
 
     if (this !== e.target) {
       //console.log(this.offsetLeft);
-      console.log(this, e.target.getBoundingClientRect().left);
+      //console.log(this, e.target.getBoundingClientRect().left);
       //console.log(e.target);
-        //x = x + e.target.offsetLeft;
-        //y = y + e.target.offsetTop;
+        x = x + e.target.offsetLeft;
+        y = y + e.target.offsetTop;
         //x = e.target.getBoundingClientRect();
         //y = e.target.getBoundingClientRect();
     }
@@ -21,9 +21,9 @@ function shadow(e) {
     const xWalk = Math.round(x / width * walk) - (walk / 2);
     const yWalk = Math.round(y / height * walk) - (walk / 2);
 
-    text.style.filter = `drop-shadow(${xWalk}px ${yWalk}px 1px grey)`;
+    text.style.filter = `drop-shadow(${xWalk}px ${yWalk}px 1px #444)`;
 
-    //text.style.transform = `perspective( 5000px ) rotateX( ${yWalk}deg ) rotateY( ${xWalk}deg )`
+    //text.style.transform = `perspective( 0 ) rotateX( ${xWalk}deg ) rotateY( ${yWalk}deg )`
 
     //console.log(xWalk, yWalk);
     //console.log(x, y);
@@ -31,4 +31,3 @@ function shadow(e) {
 }
 
 hero.addEventListener('mousemove', shadow);
-**/
