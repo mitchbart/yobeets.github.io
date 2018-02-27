046ea7b5d20733a77cc6460c4a06ec37575f6af4
Mitchell Bartolo <mitchell.bartolo@gmail.com>
2018-02-27 13:09:46 +1000

    n a site get more perfect?

--- js/functions.js
+++ js/functions.js
@@ -1,15 +1,20 @@
 
-//for loading thing
+//for unneccessary loading text(nothing is loading)
 var loadObj = { loading: '0%' };
 
 var JSobject = anime({
   targets: loadObj,
-  loading: '100%',
+  loading: '1000%',
   round: 1,
   easing: 'linear',
+  duration: 200000,
   update: function() {
     var el = document.querySelector('#loading pre');
     el.innerHTML = JSON.stringify(loadObj);
   },
   loop: true
 });
+
+//fancy word thing
+var element = document.querySelector("h2")
+charming(element)
