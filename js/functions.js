cf24118cc82c79aa569b94b3a3a40ef6d19ccda2
Mitchell Bartolo <mitchell.bartolo@gmail.com>
2018-02-28 11:25:13 +1000

    some colour changes

--- js/functions.js
+++ js/functions.js
@@ -1,20 +1,57 @@
 
 //for unneccessary loading text(nothing is loading)
-var loadObj = { loading: '0%' };
+var loadObj = { coolnessLevel: '0%' };
 
 var JSobject = anime({
   targets: loadObj,
-  loading: '1000%',
+  coolnessLevel: '1000%',
   round: 1,
   easing: 'linear',
-  duration: 200000,
+  duration: 20000,
   update: function() {
     var el = document.querySelector('#loading pre');
     el.innerHTML = JSON.stringify(loadObj);
-  },
-  loop: true
+  }
+  //loop: true
 });
 
+
+
+
+
+
 //fancy word thing
-var element = document.querySelector("h2")
-charming(element)
+let pageTitle = document.querySelector(".title")
+charming(pageTitle)
+
+console.log(pageTitle);
+
+//get random letter
+var randomChar = pageTitle.innerText[Math.floor(Math.random()*pageTitle.innerText.length)];
+
+console.log(randomChar);
+
+//array of colours 
+var colorArray = ["#FFC312", "#C4E538", "#12CBC4", "#FDA7DF", "#ED4C67"];
+//var randomColor = colorArray[Math.floor(Math.random()*colorArray.length)];
+
+function getRandomColor () {
+  return colorArray[Math.floor(Math.random()*colorArray.length)];
+};
+
+//need to get rnadom span element in page title
+
+function getRandomNumber () {
+  return Math.floor(Math.random()*pageTitle.length);
+};
+console.log(getRandomNumber());
+
+
+//some code
+function color (heading) {
+  heading.style.color = getRandomColor();
+};
+
+setInterval(color, 300, pageTitle);
+
+//console.log(randomColor);
