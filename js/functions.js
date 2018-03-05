3448e8483281525c3c438ba2e08efdd4cd8f6364
Mitchell Bartolo <mitchell.bartolo@gmail.com>
2018-03-05 14:14:33 +1000

    updated pizza mode# Please enter the commit message for your changes. Lines starting

--- js/functions.js
+++ js/functions.js
@@ -15,14 +15,23 @@ var JSobject = anime({
   //loop: true
 });
 
+//special drip effect whatever that is?
+//var dripping = document.querySelector('.drip');
 
-
+//var domNode = anime({
+ // targets: dripping,
+//  translateY: -25
+//});
 
 
 
 //fancy word thing
-let pageTitle = document.querySelector(".title")
-charming(pageTitle);
+let pageTitle = document.querySelector(".party-mode");
+//charming(pageTitle);
+charming(pageTitle, {
+  classPrefix: 'drip char'
+})
+
 
 //all for title colour change
 var colorArray = ["#FFC312", "#C4E538", "#12CBC4", "#FDA7DF", "#ED4C67", "#ef5777", "#575fcf", "#4bcffa", "#34e7e4", "#34e7e4", "#0be881"];
@@ -40,6 +49,50 @@ function changeColor () {
   let c = getRandomColor();
   let x = document.querySelector(".char" + n);
   x.style.color = c;
+  
 };
 
+/**function drip() {
+  let n = getRandomNumber();
+  let x = document.querySelector(".char" + n);
+  x.classList.toggle('drip');
+  var dripping = document.querySelector('.drip');
+
+  var domNode = anime({
+    targets: dripping,
+    translateY: 20,
+    direction: 'alternate'
+});
+  console.log("wooo ladd");
+}**/
+
+
+/**var dripping = document.querySelector('.drip');
+
+  var domNode = anime({
+    targets: dripping,
+    translateY: 20,
+    direction: 'alternate'
+  });
+**/
+
 setInterval(changeColor, 10);
+//setInterval(drip, 100);
+
+/**var dripping = document.querySelectorAll('.drip');
+
+  var domNode = anime.timeline({
+    targets: dripping,
+    translateY: 20,
+    loop: true
+  });**/
+
+/**var functionBasedDelay = anime({
+  targets: '.drip',
+  translateY: 20,
+  direction: 'alternate',
+  loop: true,
+  delay: function(el, i, l) {
+    return i * 100;
+  }
+});**/
